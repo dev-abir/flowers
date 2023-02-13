c726a12dc995da356d5cb061ec4554dcb78b41d1
Abir Ganguly <gangulyabir777@gmail.com>
2023-02-13 10:39:27 +0530

    bugfix

--- flowers_server/flowers_server_project/urls.py
+++ flowers_server/flowers_server_project/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path("", include("flowers_server_app.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
